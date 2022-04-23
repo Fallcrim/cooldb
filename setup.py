3fddc97e59e157d985591aa363ecd26f71231832
Fallcrim <80700363+Fallcrim@users.noreply.github.com>
2022-04-23 15:19:08 +0200

    Added README and setup.py

---
+++ setup.py
@@ -0,0 +1,30 @@
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+VERSION = '1.0.0'
+DESCRIPTION = 'A simple database handler for SQLite3'
+LONG_DESCRIPTION = open(os.path.join(here, 'README.md')).read()
+
+# Setting up
+setup(
+    name="cooldb",
+    version=VERSION,
+    author="Fallcrim",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'db', 'database', 'wrapper', 'sqlite3'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
